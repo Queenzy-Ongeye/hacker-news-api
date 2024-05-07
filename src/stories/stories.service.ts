75b1968cd357e7e87b8a1d7b598068b8e11d8ca3
Queenzy-Ongeye <queenzyblack176@gmail.com>
2024-05-07 23:54:42 +0300

    added comments and fetchStories function

--- src/stories/stories.service.ts
+++ src/stories/stories.service.ts
@@ -3,18 +3,28 @@ import { Injectable } from '@nestjs/common';
 import { AxiosInstance } from 'axios';
 @Injectable()
 export class StoriesService {
-    private axios : AxiosInstance;
+    private axios: AxiosInstance;
 
-    constructor(private httpService: HttpService){
+    constructor(private httpService: HttpService) {
         this.axios = httpService.axiosRef;
     };
 
-    async fetchTopStories():Promise<any[]>{
+    // fetching the top 25 stories
+    async fetchTopStories(): Promise<any[]> {
         const { data: topStoryIds } = await this.axios.get("https://hacker-news.firebaseio.com/v0/topstories.json");
-        return topStoryIds.slice(0,25);
+        return topStoryIds.slice(0, 25);
     };
 
-    extractWords(titles: string[]): Record<string, number>{
+    // function of fetching titles from storyIds
+    async fetchTitlesFromStoryIds(storyIds: number[]): Promise<string[]> {
+        const stories = await Promise.all(
+            storyIds.map(id => this.axios.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`))
+        );
+        return stories.map(story => story.data.title)
+    }
+
+    // extracting words from titles
+    extractWords(titles: string[]): Record<string, number> {
         const wordCount = {}
         titles.forEach(title => {
             const words = title.split(/\s+/);
@@ -25,11 +35,12 @@ export class StoriesService {
         return wordCount;
     };
 
+    // getting the top words
     getTopWords(wordCount: Record<string, number>, limit: number = 10): string[] {
         return Object.entries(wordCount)
-        .sort((a,b) => b[1] - a[1])
-        .slice(0, limit)
-        .map(entry => entry[0]);
+            .sort((a, b) => b[1] - a[1])
+            .slice(0, limit)
+            .map(entry => entry[0]);
     };
 
 }
