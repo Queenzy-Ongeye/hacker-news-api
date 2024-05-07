cb62ff25e7a63732b88bf21778bbe1128158fa96
Queenzy-Ongeye <queenzyblack176@gmail.com>
2024-05-07 23:53:45 +0300

    added getTopWordsFromStories function

--- src/stories/stories.controller.ts
+++ src/stories/stories.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { StoriesService } from './stories.service';
 
 @Controller('stories')
-export class StoriesController {}
+export class StoriesController {
+    constructor(private storiesService: StoriesService) { }
+    @Get("top-words")
+    async getTopWordsFromStories() {
+        try {
+            const storyIds = await this.storiesService
+                .fetchTopStories();
+            const titles = await this.storiesService.fetchTitlesFromStoryIds(storyIds);
+            const wordCount = this.storiesService.extractWords(titles);
+            const topStories = this.storiesService.getTopWords(wordCount)
+            return { data: topStories };
+        }
+        catch (error){
+            return {error: "Failed to load data"}
+        }
+    }
+}
