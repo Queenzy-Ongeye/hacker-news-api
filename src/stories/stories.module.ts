869135a7574e8180a115b7216abce061c04a759e
Queenzy-Ongeye <queenzyblack176@gmail.com>
2024-05-07 23:07:21 +0300

    added HttpModule

--- src/stories/stories.module.ts
+++ src/stories/stories.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { StoriesService } from './stories.service';
 import { StoriesController } from './stories.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   providers: [StoriesService],
   controllers: [StoriesController]
 })
