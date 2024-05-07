5fb40658653f33f3ef3382d70bfefd09e596b515
Queenzy-Ongeye <queenzyblack176@gmail.com>
2024-05-07 22:30:44 +0300

    created fetchTopStories function to fetch data

--- src/stories/stories.service.ts
+++ src/stories/stories.service.ts
@@ -1,4 +1,19 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
-
+import { AxiosInstance } from 'axios';
 @Injectable()
-export class StoriesService {}
+export class StoriesService {
+    private axios : AxiosInstance;
+
+    constructor(private httpService: HttpService){
+        this.axios = httpService.axiosRef;
+    };
+
+    async fetchTopStories():Promise<any[]>{
+        const { data: topStoryIds } = await this.axios.get("https://hacker-news.firebaseio.com/v0/topstories.json");
+        return topStoryIds.slice(0,25);
+    };
+
+  
+
+}
