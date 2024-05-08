90fdd6ffba55c7a360e005d9cc729ff77e53a73d
Queenzy-Ongeye <queenzyblack176@gmail.com>
2024-05-08 13:29:47 +0300

    created post interface

--- src/stories/stories.service.ts
+++ src/stories/stories.service.ts
@@ -1,6 +1,13 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { AxiosInstance } from 'axios';
+import { response } from 'express';
+
+interface Post {
+    id: number;
+    title: string;
+    time: number; // assuming this is the timestamp of the post
+}
 @Injectable()
 export class StoriesService {
     private axios: AxiosInstance;
@@ -9,19 +16,22 @@ export class StoriesService {
         this.axios = httpService.axiosRef;
     };
 
-    // fetching the top 25 stories
-    async fetchTopStories(): Promise<any[]> {
+    // Top 10 most occurring words in the titles of the last 25 stories ---start
+    async fetchTopStories(): Promise<number[]> {
         const { data: topStoryIds } = await this.axios.get("https://hacker-news.firebaseio.com/v0/topstories.json");
         return topStoryIds.slice(0, 25);
     };
 
     // function of fetching titles from storyIds
-    async fetchTitlesFromStoryIds(storyIds: number[]): Promise<string[]> {
+    async fetchTitlesFromStoryIds(storyIds: any[]): Promise<string[]> {
         const stories = await Promise.all(
             storyIds.map(id => this.axios.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`))
         );
         return stories.map(story => story.data.title)
     }
+
+    // Top 10 most occurring words in the titles of the post of exactly the last week 3 -----Start
+    
 
     // extracting words from titles
     extractWords(titles: string[]): Record<string, number> {
