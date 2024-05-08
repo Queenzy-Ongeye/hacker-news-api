d988bd557dfade64a6b34b0eff3feefbf11c2f3c
Queenzy-Ongeye <queenzyblack176@gmail.com>
2024-05-08 14:18:37 +0300

    added getTopThreeWeekPosts() controller

--- src/stories/stories.controller.ts
+++ src/stories/stories.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus } from '@nestjs/common';
 import { StoriesService } from './stories.service';
 
 @Controller('stories')
@@ -12,10 +12,25 @@ export class StoriesController {
             const titles = await this.storiesService.fetchTitlesFromStoryIds(storyIds);
             const wordCount = this.storiesService.extractWords(titles);
             const topStories = this.storiesService.getTopWords(wordCount)
-            return { data: topStories };
+            return { topStories: topStories };
         }
-        catch (error){
-            return {error: "Failed to load data"}
+        catch (error) {
+            return { error: "Failed to load data" }
+        }
+    }
+    @Get("top-last-posts")
+    async getTopThreeWeekPosts() {
+        try {
+            const postTitles = await this.storiesService.fetchLastThreeWeekTitles();
+            if (!postTitles.length) {
+                throw new HttpException("No posts found from the last three weeks", HttpStatus.NOT_FOUND);
+            }
+            const wordCount = await this.storiesService.extractWords(postTitles);
+            const topPosts = await this.storiesService.getTopWords(wordCount)
+            return { lastThreeWeeksPosts: topPosts };
+
+        } catch (error) {
+            throw new HttpException("Failed to fetch posts", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
