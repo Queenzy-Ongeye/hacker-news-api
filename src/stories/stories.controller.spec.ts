09437ee93c6eec295274986437b5bf89dc97913c
Queenzy-Ongeye <queenzyblack176@gmail.com>
2024-05-09 12:45:28 +0300

    added fetchTitlesFromStoryIds test function

--- src/stories/stories.controller.spec.ts
+++ src/stories/stories.controller.spec.ts
@@ -1,15 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StoriesController } from './stories.controller';
+import { StoriesService } from './stories.service';
+import { HttpModule } from '@nestjs/axios';
 
 describe('StoriesController', () => {
   let controller: StoriesController;
+  let storyService: StoriesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [StoriesController],
+      providers: [StoriesService],
+      imports: [HttpModule]
     }).compile();
 
     controller = module.get<StoriesController>(StoriesController);
+    storyService = module.get<StoriesService>(StoriesService);
   });
 
   it('should be defined', () => {
