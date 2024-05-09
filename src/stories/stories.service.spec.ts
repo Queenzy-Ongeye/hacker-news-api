5d68cbee7eab17a178823f5f0ac41fe8807a56fb
Queenzy-Ongeye <queenzyblack176@gmail.com>
2024-05-09 12:45:58 +0300

    added fetchTitlesFromStoryIds test function

--- src/stories/stories.service.spec.ts
+++ src/stories/stories.service.spec.ts
@@ -1,7 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StoriesService } from './stories.service';
 import { HttpService } from '@nestjs/axios';
-import { AxiosResponse } from 'axios';
+import { Axios, AxiosResponse } from 'axios';
+import { response } from 'express';
 
 describe('StoriesService', () => {
   let service: StoriesService;
@@ -11,8 +12,8 @@ describe('StoriesService', () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [StoriesService, {
         provide: HttpService,
-        useValue:{
-          axiosRef:{
+        useValue: {
+          axiosRef: {
             get: jest.fn(),
           }
         }
@@ -28,8 +29,8 @@ describe('StoriesService', () => {
     expect(httpService).toBeDefined();
   });
 
-  describe('fetchTopStories', () =>{
-    it("should return an array of story IDs", async() =>{
+  describe('fetchTopStories', () => {
+    it("should return an array of story IDs", async () => {
       const results: AxiosResponse = {
         data: [111, 222, 333, 444, 555],
         status: 200,
@@ -43,7 +44,32 @@ describe('StoriesService', () => {
 
       expect(await service.fetchTopStories()).toEqual(results.data.slice(0, 25));
       expect(httpService.axiosRef.get).toHaveBeenCalledWith("https://hacker-news.firebaseio.com/v0/topstories.json")
+    });
+  });
 
-    })
-  })
+  describe("fetchTitlesFromStoryIds", () => {
+    it("should return an array of titles from story Ids", async () => {
+      const storyIds = [111, 222, 333, 444, 555]
+      const result = storyIds.map(id => (
+        {
+          data: { id, title: `Title for story id ${id}` },
+          status: 200,
+          statusText: "OK",
+          headers: {},
+          config: {
+            headers: undefined
+          },
+        }
+      ))
+      jest.spyOn(httpService.axiosRef, "get").mockImplementation(
+        url => Promise.resolve(result.find(response => url.includes(response.data.id.toString())))
+      )
+
+      const titles = await service.fetchTitlesFromStoryIds(storyIds)
+      expect(titles).toEqual(result.map(response => response.data.title))
+      storyIds.map(id => {
+        expect(httpService.axiosRef.get).toHaveBeenCalledWith(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
+      })
+    });
+  });
 });
